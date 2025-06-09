4570b6c6223e5ff992569e0ca845b702f44aac7f
Erica Valentine <epv1613@gmail.com>
2025-06-09 09:46:21 -0400

    fixed pages

--- public/saved songs page.js
+++ public/saved songs page.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './SavedSongs.css';
 
 const SavedSongs = () => {
     const [songs, setSongs] = useState([]);
